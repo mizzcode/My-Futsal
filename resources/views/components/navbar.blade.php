b4c31f6b3e570c8f4c29a75823faf0ce4b616463
Misbahudin <misbahudin.id@gmail.com>
2025-01-06 10:02:00 +0700

    feat: add view navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,31 @@
+<div class="min-h-full">
+    <nav class="bg-white">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 items-center justify-between">
+                <div class="flex items-center">
+                    <img class="size-10" src="{{ asset('storage/image-removebg.png') }}" alt="My Futsal Logo">
+                    <span>My Futsal</span>
+                </div>
+                <div class="hidden md:block">
+                    <div class="ml-4 flex items-center md:ml-6">
+                        <a href="#" class="text-gray-900 hover:text-gray-900"><span class="fw-bold">HOME</span></a>
+                        <a href="#" class="ml-4 hover:text-gray-900"><span class="fw-bold">VENUE</span></a>
+                        <button class="bg-red-500 ms-2 rounded-xl text-white py-2 px-3">LOGIN</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <header class="bg-[#252525] shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-white">Dashboard</h1>
+            {{-- ini dropwdown dan nanti h1 hapus aja --}}
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+        </div>
+    </main>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('header')
+  @include('components.navbar')
+@endsection
