7a6abf8723d69e65fe937eb3ad4c2675de4eb0df
Misbahudin <misbahudin.id@gmail.com>
2025-01-09 10:39:15 +0700

    feat(ui): add layout app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,20 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script>
 </head>
 
 <body>
-    @yield('header')
+    <div class="min-h-full mb-10">
+        @yield('header')
 
-    <main>
-        @yield('content')
-    </main>
+        <main>
+            @yield('content')
+        </main>
 
-    <footer>
-        @yield('footer')
-    </footer>
+        <footer>
+            @yield('footer')
+        </footer>
+    </div>
 </body>
 
 </html>
