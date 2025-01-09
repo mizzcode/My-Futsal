135dcea669b8754b66bd26509062b199632fb367
Misbahudin <misbahudin.id@gmail.com>
2025-01-09 10:41:46 +0700

    feat(ui): add banner component with motivational message and image

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,7 @@
+<div class="relative w-full h-[409px] mt-14">
+  <img class="w-full h-full object-cover rounded-2xl" src="{{ asset('storage/banner.png') }}" alt="banner">
+  <div class="absolute inset-0 flex flex-col justify-center text-white mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <h1 class="text-6xl font-bold">Move Sweat Thrive</h1>
+      <p class="mt-5 text-xl w-3/5">Embrace each day with the strength to move, the courage to push limits, and the determination to be better. Every workout is a step towards a healthier, happier you.</p>
+  </div>
+</div>
