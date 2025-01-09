06c14523fcde56b091e2d9e53d3d6742d768f64d
Misbahudin <misbahudin.id@gmail.com>
2025-01-09 10:41:55 +0700

    feat(ui): add footer component with company info and social links

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,53 @@
+<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="grid grid-cols-4 my-14">
+        <div class="col-span-1 mt-5">
+            <h1 class="font-bold text-lg">COMPANY</h1>
+            <ul class="mt-3 text-black-46 font-semibold">
+                <li class="flex text-black items-center">
+                    <img class="size-10" src="{{ asset('storage/image-removebg.png') }}" alt="My Futsal Logo">
+                    MY FUTSAL
+                </li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Carer</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+        </div>
+        <div class="col-span-1 mt-5">
+            <h1 class="font-bold text-lg">PRODUCT</h1>
+            <ul class="mt-3 text-black-46 font-semibold">
+                <li><a href="#">Venue</a></li>
+                <li><a href="#">Community</a></li>
+            </ul>
+        </div>
+        <div class="col-span-1 mt-5">
+            <h1 class="font-bold text-lg">SUPPORT</h1>
+            <ul class="mt-3 text-black-46 font-semibold">
+                <li><a href="#">API Reference</a></li>
+                <li><a href="#">Insurance</a></li>
+                <li><a href="#">Help Center</a></li>
+                <li><a href="#">Live Chat</a></li>
+            </ul>
+        </div>
+        <div class="col-span-1 mt-5">
+            <h1 class="font-bold text-lg mx-auto">Our Logo</h1>
+            <img class="-ms-7" src="{{ asset('storage/image-removebg2.png') }}" alt="logo">
+        </div>
+    </div>
+
+    <div class="flex items-center justify-between">
+        <div class="flex flex-col">
+            <h1 class="font-bold text-lg">PT. YUNTA MANDIRI GROUP</h1>
+            <p class="text-black-46 font-semibold mt-3">&copy; 2024 MY FUTSAL All Right Reserved <span class="px-5"><a
+                        href="#">Privacy Policy</a></span><span><a href="#">Legal</a></span></p>
+        </div>
+        <div class="flex flex-col">
+            <div class="flex items-center gap-5">
+                <a href="#"><img src="{{ asset('storage/social.png') }}" alt="instagram"></a>
+                <a href="#"><img src="{{ asset('storage/linkedin.png') }}" alt="linkedin"></a>
+                <a href="#"><img src="{{ asset('storage/play.png') }}" alt="play"></a>
+                <a href="#"><img src="{{ asset('storage/facebook.png') }}" alt="facebook"></a>
+            </div>
+            <p class="text-black-46 font-semibold mt-3">support@myfutsal<span class="ps-5"><a href="#">+62 812 8655 8895</a></span></p>
+        </div>
+    </div>
+</div>
