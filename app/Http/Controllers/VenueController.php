d1ae1957ab0155f077051d8f3769ea3687b31458
Misbahudin <misbahudin.id@gmail.com>
2025-02-01 00:33:23 +0700

    chore: remove unnecessary log statement and add newline in index method

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -10,14 +10,13 @@ class VenueController extends Controller
 {
     public function index() {
         $venues = Venue::latest()->paginate(6);
+        
         return view('venue', compact('venues'));
     }
 
     public function detail($slug)
     {
         $venue = Venue::where('slug', $slug)->with(['fields.schedules', 'sports', 'galleries'])->firstOrFail();
-
-        Log::info($venue);
 
         return view('detail', compact('venue'));
     }
