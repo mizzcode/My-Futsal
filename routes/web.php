fc8459eb58a3955c5d84c93b516c6d618e9a6147
Misbahudin <misbahudin.id@gmail.com>
2025-02-01 01:29:14 +0700

    feat(routes): add field detail route and move verification routes under auth group

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FieldController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\VenueController;
 use Illuminate\Support\Facades\Route;
@@ -8,15 +9,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/venue', [VenueController::class, 'index'])->name('venue');
-
 Route::get('/v/{slug}', [VenueController::class, 'detail'])->name('venue.detail');
+
+Route::get('/field/{id}', [FieldController::class, 'detail'])->name('field.detail');
 
 Route::group(['prefix' => 'auth'], function () {
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register'])->name('postRegister');
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('postLogin');
+    Route::get('/verify-register', [AuthController::class, 'showVerifyRegister'])->name('verifyRegister');
+    Route::post('/verify-code', [AuthController::class, 'verifyCode'])->name('verify-code');
 });
-
-Route::get('/verify-register', [AuthController::class, 'showVerifyRegister'])->name('verifyRegister');
-Route::post('/verify-code', [AuthController::class, 'verifyCode'])->name('verify-code');
